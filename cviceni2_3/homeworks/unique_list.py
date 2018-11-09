2d27b41fc36fec2c1fdb29422ad60d963caa38eb
Jan Dvorak <dvoraj75@gmail.com>
2018-11-09 22:43:10 +0100

    First homework

---
+++ cviceni2_3/homeworks/unique_list.py
@@ -0,0 +1,20 @@
+"""
+Doplnte kod tak aby fungoval :)
+
+Vstup: tuple s n prvky
+vystup: list s unikatnimi prvky, tzn. kazde cislo je tam max jednou
+
+je vice zpusobu jak to napsat :)
+Odmenim ty sikovne co to zvladnout na 1 radek :)
+
+hint: https://docs.python.org/3/tutorial/datastructures.html#sets
+"""
+
+
+def get_unique_list(*args):
+    pass
+
+
+test_list = (1, 1, 1, 2, 2, 2, 3, 4, 4, 5)
+assert get_unique_list() == [1, 2, 3, 4, 5]
+
